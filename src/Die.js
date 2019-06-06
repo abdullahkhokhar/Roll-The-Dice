801537e6848a76c58db10b07f6862f7be04bdb87
Abdullah Khokhar <abdullah.khokhar@mail.utoronto.ca>
2019-06-06 16:37:25 -0400

    dice component completed

--- src/Die.js
+++ src/Die.js
@@ -1,11 +1,12 @@
 // an individual die that takes props and displays the correct face of the die based on props.
 import React, {Component} from 'react';
+import './Die.css';
 
 class Die extends Component{
   render(){
     return(
       // need to render an individuaal die
-      <i className = {`fas fa-dice-${this.props.face}`}></i>
+      <i className = {`Die fas fa-dice-${this.props.face}`}></i>
 
     );
   }
--- src/RollDice.js
+++ src/RollDice.js
@@ -1,3 +1,19 @@
 // a parent component (rendered by App) that renders the dice and a button to roll.
 // renders two die components and a button
 // a die just takes in props has no states.
+
+import React, {Component} from 'react'
+
+
+class RollRice extends Component{
+  
+
+
+
+
+
+
+
+
+
+}
